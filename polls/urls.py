e2139ab4f0fa19b7dbf0094d1d0554301056de91
cooh <admin@sarinform.ru>
2017-12-27 08:52:06 +0400

    adds views test

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	# /polls/5
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+	# /polls/5/results/
+	url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+	# /polls/5/vote/
+	url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
 	return HttpResponse("Hello pshtet")
+
+
+def detail(request, question_id):
+	return HttpResponse('Вы смотрите на вопрос %s.' % question_id)
+
+
+def results(request, question_id):
+	response = "Вы смотрите на результат опроса %s."
+	return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+	return HttpResponse("Вы голосуете за жтот вопрос %s" % question_id)
