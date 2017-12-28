1e283e461acd1920a698de8c9e7cbe75cb6071b9
spermspace <admin@sarinform.ru>
2017-12-28 11:23:29 +0400

    adding linked objects

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from polls.models import Question
+from polls.models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline): #покажите 3 формы для добавления вариантов ответа
+	model = Choice
+	extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -8,5 +13,8 @@ class QuestionAdmin(admin.ModelAdmin):
 		(None, {'fields': ['question_text']}),
 		('Информация о дате', {'fields': ['pub_date'], 'classes':['collapse']}),
 	]
+	inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
